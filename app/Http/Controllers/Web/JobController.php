052b1e1b3ea3c08de6c6f55a59d54399c6bbceca
Shimanto <moynulislamshimanto24@gmail.com>
2026-09-16 12:44:10 +0600

    Enhance job sharing functionality; include job requirements and responsibilities in share message

--- app/Http/Controllers/Web/JobController.php
+++ app/Http/Controllers/Web/JobController.php
@@ -109,9 +109,17 @@ class JobController extends AppBaseController
         $shareTitle = html_entity_decode(strip_tags($job->job_title), ENT_QUOTES | ENT_HTML5, 'UTF-8');
         $shareText = $companyName !== '' ? $shareTitle.' - '.$companyName : $shareTitle;
         $location = trim((string) $job->district_thana_location);
+        $shareRequirements = collect([$job->description, $job->key_responsibilities])
+            ->filter()
+            ->map(function ($content) {
+                return preg_replace('/\s+/', ' ', trim(html_entity_decode(strip_tags($content), ENT_QUOTES | ENT_HTML5, 'UTF-8')));
+            })
+            ->filter()
+            ->implode(' ');
         $shareDescription = 'Apply for '.$shareTitle;
         $shareDescription .= $companyName !== '' ? ' at '.$companyName : '';
         $shareDescription .= $location !== '' ? ' in '.$location : '';
+        $shareDescription .= $shareRequirements !== '' ? '. '.$shareRequirements : '';
         $shareDescription = Str::limit($shareDescription.'. View job requirements, responsibilities and apply online on '.getAppName().'.', 200);
         $shareMessage = $shareText."\n".$shareDescription."\n".$shareUrl;
 
